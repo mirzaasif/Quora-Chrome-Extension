a09504d326e0ccd53779d78bbe3ad6f7200b24ac
mirzaasif <hhhhbk@hotmail.com>
2012-03-10 11:53:42 +0600

    Notification shown, login check, search from pop up, post from popup, notification counter, message counter done
    Message notification could not test

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,59 @@
+var result = null;
+
 function openLink(link)
 {
 	url = "http://www.quora.com/" + link;
 	create = {"url": url};
-	
-	chrome.tabs.create(create)
+	chrome.tabs.create(create);
+	hide();
 }
 
-function updateMenu(login)
+function openFullLink(link)
 {
-	if(login)
+	create = {"url": link};
+	chrome.tabs.create(create);
+	hide();
+}
+
+function postThisPage()
+{
+	sendMessage({"data":"post"}, function(){});	
+	hide();
+}
+
+function hide()
+{
+	var views = chrome.extension.getViews({});
+    for (var corey = 0; corey < views.length; corey++) 
+    {		//Find pop.html in all of those pages
+        if (views[corey].location.href == chrome.extension.getURL('popup.html')) 
+        {
+        	views[corey].close();					
+         	break;												
+        }
+    }
+}
+
+function search()
+{
+	query = $("#searchInput").val();
+	if(query == "")
+	{
+		return;
+	}
+	
+	sendMessage({"data":"search", "query":query}, function(){});
+	hide();
+}
+
+function showNotifications()
+{
+	
+}
+
+function update()
+{
+	if(result != null)
 	{
 		$("#item_login").css("display", "none");
 		$("#item_about").css("display", "none");
@@ -17,6 +62,32 @@ function updateMenu(login)
 		$("#item_notification").css("display", "block");
 		$("#item_inbox").css("display", "block");
 		$("#item_profile").css("display", "block");
+		
+		notification = parseInt(result.notifs.unseen_aggregated_count);
+		inbox = parseInt(result.inbox.unread_count);
+		
+		if(inbox > 0)
+		{
+			$("#item_inbox_count").css("display", "block");
+			$("#item_inbox_count").text(inbox);
+		}else
+		{
+			$("#item_inbox_count").css("display", "none");	
+		}
+		
+		if(notification > 0)
+		{
+			$("#item_notification_count").css("display", "block");
+			$("#item_notification_count").text(notification);
+			showNotifications();
+			
+		}else
+		{
+			$("#item_notification_count").css("display", "none");
+			$(".notifications").css("display", "none");
+			$("#notification_content").html("");
+		}
+		
 	}else
 	{
 		$("#item_login").css("display", "block");
@@ -25,16 +96,55 @@ function updateMenu(login)
 		
 		$("#item_notification").css("display", "none");
 		$("#item_inbox").css("display", "none");
-		$("#item_profile").css("display", "none");	
+		$("#item_profile").css("display", "none");
+		
+		$("#item_notification_count").css("display", "none");
+		$("#item_inbox_count").css("display", "none");	
 	}
+}
+
+function showNotifications()
+{
+	var html = "";
+	for (i = 0; i < result.notifs.unseen.length && i < 2; i++)
+	{
+		html += "<div class='separator'>&nbsp;</div>";
+		html += "<div>"+"<a href=\"http://www.quora.com/Animesh-Kumar-1\" routing=\"q://user/(1073247)\">Animesh Kumar</a> wrote an answer for the question <a action=\"notificationSelected:\" href=\"http://www.quora.com/Codesprint-Quora/What-was-it-like-to-participate-in-Codesprint-Quora-2012/answer/Animesh-Kumar-1?__snids__=36380858#ans1069374\" arg=\"{&quot;aid&quot;: 1069374, &quot;notif_key&quot;: 7, &quot;snid&quot;: 36380858}\">Codesprint Quora:&nbsp;What was it like to participate in Codesprint Quora 2012?</a>"+"</div>"
+		//html += "<div>"+result.notifs.unseen[i]+"</div>";
+		html += "<div class='separator'>&nbsp;</div>";
+	}	
+	
+	$("#notification_content").html(html);
+	$(".notifications").css("display", "block");
+	
+	$("#notification_content a").each(
+		function()
+		{
+			$(this).click(function(){openFullLink($(this).attr("href"))});
+		}
+	);
 }
 
 function showProfile()
 {
-	updateMenu(false);
+	if(result != null)
+	{
+		create = {"url": result.link};
+		chrome.tabs.create(create);
+		hide();	
+	}
+}
+
+function sendMessage(message, response)
+{
+	chrome.extension.sendRequest(message, response);
 }
 
 function onLoad()
 {
+	sendMessage({"data": "login"}, function(response) {
+  		result = response.result;
+	  	update();
+	});
 	chrome.browserAction.setTitle({"title": "Quora Charm"})
 }
