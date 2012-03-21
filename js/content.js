e1bbe2bc45419dc34c3bcf90a8b02843cc3414ba
mirzaasif <hhhhbk@hotmail.com>
2012-03-21 22:14:36 +0600

    Some css update for making compatible with some special sites

--- js/content.js
+++ js/content.js
@@ -9,7 +9,7 @@ function onLoad()
 		closeIcon = chrome.extension.getURL("images/close.png");
 		blockIcon = chrome.extension.getURL("images/block.png");
 		//$("body").append("<div class='charm_quora' id='charm_quora'><div class='title'>Recommended Boards on Quora</div><div id='result' class='result'></div><div style='text-align:center; padding-bottom:4px;'><img src='"+closeIcon+"' width='16' title='Hide' style='cursor:pointer;' id='charm_hide'/>&nbsp;&nbsp;<img src='"+blockIcon+"' width='16' title='Never show recommendations on this site.' style='cursor:pointer;' id='charm_block'/></div><div style='text-align:center; color:#666; font-size:10px; clear:both;'>Charm for Quora</div></div>");
-		$("body").append("<div class='charm_quora' id='charm_quora'><div class='title'>Recommended Boards on Quora</div><div id='result' class='result'></div><div style='text-align:center; padding-bottom:4px;'><label id='charm_post' class='link' style=' cursor:pointer;' title='Post this page to a board on Quora'>Post</label><span style='color:#CCC;'> • </span><label id='charm_hide' class='link_alt' style=' cursor:pointer;' title='Hide this recommendation'>Hide</label></div><div style='text-align:center; padding-bottom:4px; cursor:pointer;' class='link_alt' id='charm_block' title='Add the domain for this page into the recommendation block list'>Block for this site</div><div style='text-align:center; color:#666; font-size:10px; clear:both;'>Charm for Quora</div></div>");
+		$("body").append("<div class='charm_quora' id='charm_quora'><div class='charm_quora_title'>Recommended Boards on Quora</div><div id='charm_result' class='charm_result'></div><div style='text-align:center; padding-bottom:4px;'><label id='charm_post' class='charm_quora_link' style=' cursor:pointer;' title='Post this page to a board on Quora'>Post</label><span style='color:#CCC;'> • </span><label id='charm_hide' class='charm_quora_link_alt' style=' cursor:pointer;' title='Hide this recommendation'>Hide</label></div><div style='text-align:center; padding-bottom:4px; cursor:pointer; display:block;' class='charm_quora_link_alt' id='charm_block' title='Add the domain for this page into the recommendation block list'>Block for this site</div><div style='text-align:center; color:#666; font-size:10px; clear:both;'>Charm for Quora</div></div>");
 		left = (parseInt($(window).width())-150);
 		$("#charm_quora").css("left", left);
 		$("#charm_block").click(blockSite);
@@ -69,9 +69,9 @@ function handleBoardRecommendationResponse(response)
 		
 			if(url.toString().toLowerCase() != documentUrl.toString().toLowerCase())
 			{
-				div = "<a href='"+url+"' target='_blank' title='Open "+name+" in a separate tab'><div class='result_item' id='result_item_"+count+"' data-name='"+name+"'>"+name+"</div></a>";
+				div = "<a href='"+url+"' target='_blank' title='Open "+name+" in a separate tab'><div class='charm_result_item' id='result_item_"+count+"' data-name='"+name+"'>"+name+"</div></a>";
 				//div = "<div class='result_item' id='result_item_"+count+"' data-name='"+name+"'>"+name+"</div>";
-				$(".charm_quora #result").append(div);
+				$(".charm_quora #charm_result").append(div);
 				//$("#result_item_"+count).click(function(event){postQuora(event);});
 				count++;	
 			}		
